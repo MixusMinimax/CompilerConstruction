2bb74eef28d3f93ef98962808e2fea4e7f0de09c
MixusMinimax <maxi.barmetler@gmail.com>
2022-08-03 13:15:45 +0200

    Delete Ex03

--- Ex03/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿using CommandLineProject.Extensions;
-using Ex03.Services;
-using Microsoft.Extensions.DependencyInjection;
-
-await new ServiceCollection()
-    .AddSingleton<IBerrySethiService, BerrySethiService>()
-    .RegisterCommands(typeof(Program))
-    .BuildServiceProvider()
-    .RunCommandLineApp();
